841c257a4c6b9ef2c5bb2579c04e97077d2eef6c
BrenoC4rvalho <bc101004@gmail.com>
2025-05-25 17:28:50 -0300

    feat: create ollamaChatService

---
+++ src/main/java/com/example/ollamabot/service/OllamaChatService.java
@@ -0,0 +1,66 @@
+package com.example.ollamabot.service;
+
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OllamaChatService {
+
+    @Autowired
+    private ChatModel chatModel;
+
+    private final List<Message> conversationHistory = new ArrayList<>();
+
+    public ChatResponse chat(String message) {
+
+        conversationHistory.add(new UserMessage(message));
+
+        Prompt prompt = new Prompt(conversationHistory);
+        ChatResponse response = chatModel.call(prompt);
+
+        if(!response.getResults().isEmpty()) {
+            String assistantReply = response.getResults().getFirst().getOutput().getText();
+            if (assistantReply != null) {
+                conversationHistory.add(new AssistantMessage(assistantReply));
+            }
+        }
+
+        return response;
+    }
+
+    public void resetHistory() {
+        conversationHistory.clear();
+    }
+
+//    var imageResource = new ClassPathResource("/multimodal.test.png");
+
+//    var userMessage = new UserMessage("Explain what do you see on this picture?",
+//            new Media(MimeTypeUtils.IMAGE_PNG, this.imageResource));
+//
+//    ChatResponse response = chatModel.call(new Prompt(this.userMessage,
+//            OllamaOptions.builder().model(OllamaModel.LLAVA)).build());
+//
+
+//    public ChatResponse chatWithImage(String message, Resource image) {
+//        Media media = new Media(MimeTypeUtils.IMAGE_PNG, image);
+//        UserMessage userMessage = new UserMessage(message, media);
+//        conversationHistory.add(userMessage);
+//
+//        Prompt prompt = new Prompt(conversationHistory);
+//        ChatResponse response = chatModel.call(prompt);
+//        conversationHistory.add(new AssistantMessage(response.getContent()));
+//
+//        return response;
+//    }
+
+
+}
